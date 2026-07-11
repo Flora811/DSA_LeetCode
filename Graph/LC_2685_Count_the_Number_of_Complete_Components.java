94b779c12f6dbe9e7b83503237598260f01103bd
Flora811 <florentine.bhatt.333@gmail.com>
2026-07-11 19:55:55 -0400

    Approach 3: Using DSU

--- Graph/LC_2685_Count_the_Number_of_Complete_Components.java
+++ Graph/LC_2685_Count_the_Number_of_Complete_Components.java
@@ -1,46 +1,61 @@
 class Solution {
-    //TC: O(V+E) DFS
-    //SC: O(V+E) adj list
+    //TC: E * alpha (V)
+    public int find(int x, int parent[]){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x], parent);
+    }
+
+    public void union(int x, int y, int[] parent, int size[]){
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+
+        if(x_parent == y_parent) return;
+
+        if(size[x_parent] > size[y_parent]){
+            parent[y_parent] = x_parent;
+            size[x_parent] += size[y_parent];
+        }
+        else if (size[y_parent] > size[x_parent]){
+            parent[x_parent] = y_parent;
+            size[y_parent] += size[x_parent];
+        }
+        else{
+            parent[x_parent] = y_parent;
+            size[y_parent] += size[x_parent];
+        }
+    }
     public int countCompleteComponents(int n, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(i, new ArrayList<>());
+        int[] parent = new int[n];
+        //At the Start -> everyone is its own parent
+        for(int i=0; i<n; i++) parent[i] = i;
+
+        int[] size = new int[n];
+        Arrays.fill(size, 1);
+
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int e[]: edges){
-            int u = e[0];
-            int v = e[1];
-
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            union(e[0], e[1], parent, size);
         }
 
+        for(int e[]: edges){
+            int p = find(e[0], parent);
+            map.put(p, map.getOrDefault(p, 0)+1);
+        }
+
+        int ans =0;
         boolean visited[] = new boolean[n];
-        int ans=0;
-
         for(int i=0; i<n; i++){
-            if(!visited[i]){
-                // Edge | vertex
-                int evCount[] = countEdges(i, adj, visited);
-                int v = evCount[1];
-                int e = evCount[0];
-                if(v*(v-1) == e) ans++;
-            }
+            int p = parent[i];
+            if(visited[p]) continue;
+
+            visited[p] = true;
+            int v = size[p];
+            int e = map.getOrDefault(p, 0);
+
+            if( (v*(v-1)/2) == e) ans++;
         }
+
         return ans;
-    }
-    public int[] countEdges(int node, List<List<Integer>> adj, boolean visited[]){
-
-        visited[node] = true;
-
-        int edges = adj.get(node).size();
-        int vertices = 1;
-
-        for(int neigh: adj.get(node)){
-            if(!visited[neigh]){
-                int[] ev = countEdges(neigh, adj, visited);
-                edges += ev[0];
-                vertices += ev[1];
-            }
-        }
-        return new int[] {edges, vertices};
     }
 }
