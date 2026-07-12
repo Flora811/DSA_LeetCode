66d17a2fed61abf9ae179646793c59e7d69669d7
Flora811 <florentine.bhatt.333@gmail.com>
2026-07-12 07:33:35 -0400

    Approach 2 - using HashMap

--- Arrays/LC_1331_Rank_Transform_of_an_Array.java
+++ Arrays/LC_1331_Rank_Transform_of_an_Array.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
         int n = arr.length;
+        int sort[] = new int[n];
+        for(int i=0; i<n; i++) sort[i] = arr[i];
 
-        int ans[] = new int[n];
-        if(n==0) return ans;
+        Arrays.sort(sort);
 
-        //number | idx
-        int sorted[][] = new int[n][2];
-        for(int i=0; i<n; i++){
-            sorted[i][0] = arr[i];
-            sorted[i][1] = i;
-        }
-        Arrays.sort(sorted, Comparator.comparingInt(a -> a[0]));
+        Map<Integer, Integer> map = new HashMap<>();
 
         int rank = 1;
-        ans[sorted[0][1]] = 1;
-        for(int i=1; i<n; i++){
-            int idx = sorted[i][1];
-            if(sorted[i][0] == sorted[i-1][0]){
-                ans[idx] = ans[sorted[i-1][1]];
-            }
-            else{
-                ans[idx] = ++rank;
-            }
+        for(int num: sort){
+            if(!map.containsKey(num))
+                map.put(num, rank++);
         }
 
-        return ans;
+        for(int i=0; i<n; i++) sort[i] = map.get(arr[i]);
+
+        return sort;
     }
 }
