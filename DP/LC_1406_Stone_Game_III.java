29f834ab0f7430848f3941dd58a8ee013e5bd6fc
Flora811 <florentine.bhatt.333@gmail.com>
2026-08-03 08:58:39 -0400

    Approach 2 - Bottom Up

--- DP/LC_1406_Stone_Game_III.java
+++ DP/LC_1406_Stone_Game_III.java
@@ -32,3 +32,40 @@ class Solution {
         return t[i] = diff;
     }
 }
+
+/**
+    Approach 2 - Bottom Up : 
+    TC: O(n);
+    SC: O(n);
+*/
+
+class Solution {
+    int n;
+    int t[];
+    public String stoneGameIII(int[] stoneValue) {
+        this.n = stoneValue.length;
+        this.t = new int[n+1];
+
+        //Bottom Up
+        //t[n] = 0
+
+        for(int i=n-1; i>=0; i--){
+            int diff = Integer.MIN_VALUE;
+            
+            diff = Math.max(diff, stoneValue[i] - t[i+1]);
+            if(i+1 < n)
+                diff = Math.max(diff, stoneValue[i] + stoneValue[i+1] - t[i+2]);
+            if(i+2 < n)
+                diff = Math.max(diff, stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - t[i+3]);
+            
+            t[i] = diff;
+        }
+
+        //int scoreDiff = solve(0, stoneValue);
+        int scoreDiff = t[0];
+
+        if(scoreDiff > 0) return "Alice";
+        if (scoreDiff == 0) return "Tie";
+        return "Bob";
+    }
+}
